60d1bf2f88493358b54748c5fca7a935d118941f
Thedoree22 <jmr466587@gmail.com>
2025-10-15 18:41:12 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import os
+
+BOT_TOKEN = os.environ['BOT_TOKEN']
+
+# --- ძალიან მნიშვნელოვანია! ---
+# ამ ფუნქციებისთვის ბოტს სჭირდება წევრების სიის წაკითხვის უფლება
+intents = discord.Intents.default()
+intents.members = True # <<<<
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+    await bot.load_extension('welcome_cog')
+    await bot.load_extension('autorole_cog')
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(f"Error syncing commands: {e}")
+
+bot.run(BOT_TOKEN)
